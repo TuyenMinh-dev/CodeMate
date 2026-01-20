0cc903958f57ee986d03f70477d933ca5aa5de91
Wunyn <myuyen120806@gmail.com>
2026-01-20 13:22:27 +0700

    fix: RegisterController

--- src/main/java/roots/controllers/RegisterController.java
+++ src/main/java/roots/controllers/RegisterController.java
@@ -6,15 +6,21 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class RegisterController {
-    @FXML private TextField fullnameRGS;
-    @FXML private TextField usernameRGS;
-    @FXML private PasswordField passwordRGS;
-    @FXML private PasswordField checkPasswordRGS;
+    @FXML
+    private TextField fullnameRGS;
+    @FXML
+    private TextField usernameRGS;
+    @FXML
+    private PasswordField passwordRGS;
+    @FXML
+    private PasswordField checkPasswordRGS;
 
     @FXML
-    public void continueRGS(){
+    public void continueRGS() {
         String fullname = fullnameRGS.getText();
         String username = usernameRGS.getText();
         String password = passwordRGS.getText();
         String checkPassword = checkPasswordRGS.getText();
+    }
+}
 
