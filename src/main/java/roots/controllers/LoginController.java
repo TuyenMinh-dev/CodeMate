2f5daa6b5397f90db290d320b222231ee9673ccc
Wunyn <myuyen120806@gmail.com>
2026-02-03 23:08:07 +0700

    feat: create show password in login

--- src/main/java/roots/controllers/LoginController.java
+++ src/main/java/roots/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package roots.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +10,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import roots.constrant.Error;
 import roots.constrant.Message;
 import roots.constrant.Success;
@@ -24,12 +27,24 @@ import java.io.IOException;
 public class LoginController {
     @FXML private TextField usernameLG;
     @FXML private PasswordField passwordLG;
+    @FXML private TextField passwordTextLG;
     @FXML private TextField emailFG;
+    @FXML private ImageView iconEYE;
+
+    private Image closeEYE = new Image(getClass().getResourceAsStream("/assets/images/icon/closeEye.png"));
+    private Image openEYE = new Image(getClass().getResourceAsStream("/assets/images/icon/openEye.png"));
 
     @FXML private Label errorUsername;
     @FXML private Label errorPassword;
 
     private LoginDAO loginDAO = new LoginDAO();
+    private AuthServiceImpl authService = new AuthServiceImpl();
+
+    @FXML
+    public void initialize(){
+        passwordTextLG.setVisible(false);
+        iconEYE.setImage(closeEYE);
+    }
 
     @FXML
     public void loginLG(ActionEvent event){
@@ -50,7 +65,6 @@ public class LoginController {
             return;
         }
         else {
-            AuthServiceImpl authService = new AuthServiceImpl();
 
             User user = authService.login(name, pass);
             if(user != null){
@@ -63,6 +77,21 @@ public class LoginController {
             }
         }
 
+    }
+    @FXML
+    public void showPassword(Event event){
+        if(!passwordTextLG.isVisible()){
+            passwordTextLG.setText(passwordLG.getText());
+            passwordTextLG.setVisible(true);
+            passwordLG.setVisible(false);
+            iconEYE.setImage(openEYE);
+        }
+        else{
+            passwordLG.setText(passwordTextLG.getText());
+            passwordTextLG.setVisible(false);
+            passwordLG.setVisible(true);
+            iconEYE.setImage(closeEYE);
+        }
     }
     @FXML
     public void forgetLG(ActionEvent event){
